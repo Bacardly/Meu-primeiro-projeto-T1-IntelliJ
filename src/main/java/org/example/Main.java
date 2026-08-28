8a78c7a504bde594eeca11f0e6c7873a8aaec0d8
47768515803 <brunof.dasilva123@gmail.com>
2026-08-28 15:34:45 -0300

    Atividade 1 - Casting e Conversão de Tipos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,45 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+
+        // 1. Casting implícito
+        int numeroInt = 10;
+        double numeroDouble = numeroInt;
+
+        System.out.println("Exercício 1:");
+        System.out.println("Valor int: " + numeroInt);
+        System.out.println("Valor double: " + numeroDouble);
+
+
+        // 2. Casting explícito
+        double valorFracionado = 15.75;
+        int valorInteiro = (int) valorFracionado;
+
+        System.out.println("\nExercício 2:");
+        System.out.println("Valor double: " + valorFracionado);
+        System.out.println("Valor int: " + valorInteiro);
+
+
+        // 3. Conversão de tipos
+        float valorFloat = 25.50f;
+        long valorLong = (long) valorFloat;
+        double valorDouble = valorLong;
+
+        System.out.println("\nExercício 3:");
+        System.out.println("Valor float: " + valorFloat);
+        System.out.println("Valor long: " + valorLong);
+        System.out.println("Valor double: " + valorDouble);
+
+
+        // 4. Diferença entre tipos de dados
+        short valorShort = 100;
+        int valorInt = valorShort;
+        byte valorByte = (byte) valorShort;
+
+        System.out.println("\nExercício 4:");
+        System.out.println("Valor short: " + valorShort);
+        System.out.println("Valor int: " + valorInt);
+        System.out.println("Valor byte: " + valorByte);
     }
 }
