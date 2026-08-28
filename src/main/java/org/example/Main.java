27cb495ee63dce0bbc083758edd2cf4dcbcfff33
47768515803 <brunof.dasilva123@gmail.com>
2026-08-28 16:21:02 -0300

    Atividade 2 - Operadores lógicos e relacionais

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,36 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
+
+    public static void main(String[] args) {
+
+        // Exercício 1 - Comparação de dois números
+
+        int numero1 = 10;
+        int numero2 = 20;
+
+        System.out.println("Exercício 1 - Comparação de dois números:");
+
+        System.out.println("numero1 > numero2: " + (numero1 > numero2));
+        System.out.println("numero1 < numero2: " + (numero1 < numero2));
+        System.out.println("numero1 >= numero2: " + (numero1 >= numero2));
+        System.out.println("numero1 <= numero2: " + (numero1 <= numero2));
+        System.out.println("numero1 == numero2: " + (numero1 == numero2));
+        System.out.println("numero1 != numero2: " + (numero1 != numero2));
 
 
-        // 1. Casting implícito
-        int numeroInt = 10;
-        double numeroDouble = numeroInt;
+        // Exercício 2 - Operadores lógicos
 
-        System.out.println("Exercício 1:");
-        System.out.println("Valor int: " + numeroInt);
-        System.out.println("Valor double: " + numeroDouble);
+        boolean cond1 = true;
+        boolean cond2 = false;
+        boolean cond3 = true;
 
+        System.out.println("\nExercício 2 - Operadores lógicos:");
 
-        // 2. Casting explícito
-        double valorFracionado = 15.75;
-        int valorInteiro = (int) valorFracionado;
+        System.out.println("cond1 && cond2: " + (cond1 && cond2));
+        System.out.println("cond1 && cond3: " + (cond1 && cond3));
 
-        System.out.println("\nExercício 2:");
-        System.out.println("Valor double: " + valorFracionado);
-        System.out.println("Valor int: " + valorInteiro);
-
-
-        // 3. Conversão de tipos
-        float valorFloat = 25.50f;
-        long valorLong = (long) valorFloat;
-        double valorDouble = valorLong;
-
-        System.out.println("\nExercício 3:");
-        System.out.println("Valor float: " + valorFloat);
-        System.out.println("Valor long: " + valorLong);
-        System.out.println("Valor double: " + valorDouble);
-
-
-        // 4. Diferença entre tipos de dados
-        short valorShort = 100;
-        int valorInt = valorShort;
-        byte valorByte = (byte) valorShort;
-
-        System.out.println("\nExercício 4:");
-        System.out.println("Valor short: " + valorShort);
-        System.out.println("Valor int: " + valorInt);
-        System.out.println("Valor byte: " + valorByte);
+        System.out.println("cond1 || cond2: " + (cond1 || cond2));
+        System.out.println("cond2 || cond3: " + (cond2 || cond3));
     }
 }
