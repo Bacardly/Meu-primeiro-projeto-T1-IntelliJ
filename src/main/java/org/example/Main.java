745212da843366617f1492cfd953826bfd9da793
47768515803 <brunof.dasilva123@gmail.com>
2026-09-04 14:42:02 -0300

    Questão 10 - Aprovação de Empréstimo Pessoal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,18 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-        System.out.print("Tempo de empresa (meses): ");
-        int meses = sc.nextInt();
+            System.out.println("Digite seu salario: ");
+            var salario = sc.nextDouble();
 
-        if (meses > 3)
-            System.out.println("Pode aderir ao plano de saúde.");
-        else
-            System.out.println("Não pode aderir ao plano de saúde.");
-    }
+            System.out.println("Digite sua parcela desejada: ");
+            var parcela = sc.nextDouble();
+
+            if (parcela <= salario * 0.30)
+                System.out.println("Pode solicitar o emprestimo.");
+            else
+                System.out.println("Não pode solicitar o emprestimo");
+        }
 }
